03c1a94fb20a0af2bd8d0ff227b13d6d335bb1dc
Colton Randall <colton.randall@xero.com>
2024-03-01 15:10:20 +1300

    create player class

---
+++ Engine/Models/Player.cs
@@ -0,0 +1,11 @@
+namespace Engine.Models;
+
+public class Player
+{
+    private string name { get; set; } 
+    private string characterClass { get; set; } 
+    private int hitPoints { get; set; } 
+    private int xp { get; set; } 
+    private int level { get; set; } 
+    private int gold { get; set; } 
+}
